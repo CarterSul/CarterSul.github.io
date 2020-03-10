bdd02cf93f41b56998ea58caa66e02cfd42c56ef
CarterSul <62031133+CarterSul@users.noreply.github.com>
2020-03-10 11:25:50 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,41 @@
-if (LeftLoop&&XSpeed<3){
+let Y;
+let X;
+let gravity;
+let upwards;
+let YSpeed;
+let XSpeed;
+let LeftLoop=false;
+let RightLoop=false;
+let cnv;
+let h=0;
+let w=0;
+
+
+function setup() {
+  cnv = createCanvas(150, 350);
+  Y = height/2;
+  gravity=0.4;
+  XSpeed=0;
+  X = width/2;
+  YSpeed=1;
+}
+
+function draw() {
+  background(220);
+  //gravity
+  YSpeed += gravity;
+  Y += YSpeed;
+  X += XSpeed;
+  if(Y>=height-10){Y-=YSpeed;YSpeed=YSpeed*-0.7;}
+  fill(0,0,0);
+  noStroke();
+  ellipse(X,Y,20,20);
+  
+  
+  if(X-10<0||X+10>width){X-=XSpeed;XSpeed=0;}
+  
+  
+  if (LeftLoop&&XSpeed<3){
   XSpeed -= 0.07;
   }
   if (RightLoop&&XSpeed>-3){
@@ -8,7 +45,7 @@ if (LeftLoop&&XSpeed<3){
   XSpeed=XSpeed/1.01
 
   
-
+}
 
 
 
